bb5c288e297cda4acb6a5a7bb41d2049cde02c6c
JeetRoy97 <royprasenjeet97@gmail.com>
2021-09-22 19:34:47 +0530

    models saved on memory and best model picked on validation accuracy

---
+++ mnist/gamma_variation_val_accuracy.py
@@ -0,0 +1,85 @@
+
+"""
+================================
+Recognizing hand-written digits
+================================
+This example shows how scikit-learn can be used to recognize images of
+hand-written digits, from 0-9.
+"""
+
+print(__doc__)
+
+# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
+# License: BSD 3 clause
+
+# Standard scientific Python imports
+from typing import ByteString
+import matplotlib.pyplot as plt
+
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, svm, metrics
+from sklearn.model_selection import train_test_split
+
+digits = datasets.load_digits()
+
+def hyper_accuracy(digits, para):
+
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
+
+    clf = svm.SVC(gamma=para)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, digits.target, test_size=0.15, shuffle=False)
+    
+    X_train, X_val, y_train, y_val = train_test_split(
+        data, digits.target, test_size=0.15, shuffle=True)
+
+    clf.fit(X_train, y_train)
+    print("Train ", len(X_train))
+    print("Val ", len(X_val))
+    print("Test ", len(X_test))
+    print("Train accuracy",clf.score(X_train, y_train))
+    print("Val accuracy",clf.score(X_val, y_val))
+    print("Test accuracy",clf.score(X_test, y_test))
+
+def hyper(digits, para):
+
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
+
+    clf = svm.SVC(gamma=para)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        data, digits.target, test_size=0.15, shuffle=False)
+    
+    X_train, X_val, y_train, y_val = train_test_split(
+        data, digits.target, test_size=0.15, shuffle=False)
+
+    clf.fit(X_train, y_train)
+
+    predicted = clf.predict(X_val)
+
+    accuracy = metrics.accuracy_score(y_val, predicted)
+    return accuracy
+
+parames = [1e-7,1e-5,1e-3,0.01,0.1,1]
+#parames = sys.args[0]
+acc = []
+for i in range(len(parames)):
+    acc.append(hyper(digits, parames[i]))
+
+print("Gamma    ","Accuracy")
+for i in range(len(parames)):
+    print(parames[i],"     ",acc[i])
+
+plt.plot(parames, acc)
+plt.xlabel('gamma')
+plt.ylabel('accuracy')
+plt.show()
+
+best_gamma = parames[acc.index(max(acc))]
+print("optimal gamma ",best_gamma)
+
+hyper_accuracy(digits, best_gamma)
+#print("Train accuracy ",clf.score(X_train,y_train))
--- mnist/plot_digits_classification.py
+++ mnist/plot_digits_classification.py
@@ -1,41 +1,16 @@
-"""
-================================
-Recognizing hand-written digits
-================================
 
-This example shows how scikit-learn can be used to recognize images of
-hand-written digits, from 0-9.
-"""
 
-print(__doc__)
 
-# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
-# License: BSD 3 clause
-
-# Standard scientific Python imports
 import matplotlib.pyplot as plt
 import numpy as np
-# Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 import warnings
+#from skimage.transform import rescale, resize, downscale_local_mean
+from skimage.transform import rescale
 from sklearn.exceptions import DataConversionWarning
 warnings.filterwarnings(action='ignore', category=DataConversionWarning)
-###############################################################################
-# Digits dataset
-# --------------
-#
-# The digits dataset consists of 8x8
-# pixel images of digits. The ``images`` attribute of the dataset stores
-# 8x8 arrays of grayscale values for each image. We will use these arrays to
-# visualize the first 4 images. The ``target`` attribute of the dataset stores
-# the digit each image represents and this is included in the title of the 4
-# plots below.
-#
-# Note: if we were working from image files (e.g., 'png' files), we would load
-# them using :func:`matplotlib.pyplot.imread`.
-
-
+import math
 
 
 
@@ -48,123 +23,49 @@ for ax, image, label in zip(axes, digits.images, digits.target):
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
     ax.set_title('Training: %i' % label)
 
-###############################################################################
-# Classification
-# --------------
-#
-# To apply a classifier on this data, we need to flatten the images, turning
-# each 2-D array of grayscale values from shape ``(8, 8)`` into shape
-# ``(64,)``. Subsequently, the entire dataset will be of shape
-# ``(n_samples, n_features)``, where ``n_samples`` is the number of images and
-# ``n_features`` is the total number of pixels in each image.
-#
-# We can then split the data into train and test subsets and fit a support
-# vector classifier on the train samples. The fitted classifier can
-# subsequently be used to predict the value of the digit for the samples
-# in the test subset.
-
-# flatten the images
-
+def rescale_image(X, shape):
+    resized_images = []
+    for d in X:
+        resized_images.append(rescale(d, shape, anti_aliasing=False))
+    resized_images = np.asarray(resized_images)
+    resized_images = resized_images.reshape((len(X), -1))
+    return resized_images
 
 def fun(digits, shape, split):
-    n_samples = len(digits.images)
     X = digits.images
     Y = digits.target
-    #print(X.shape, Y.shape)
-    #print(type(digits.images))
-    data1 = np.resize(digits.images, (n_samples,shape))
-    #data1 = data1.reshape(n_samples, -1)
-
-    #print("data1 =", data1.shape)
-
-    # Create a classifier: a support vector classifier
-    clf = svm.SVC(gamma=0.00001)
-
-
+    data = rescale_image(X, shape)
+    #print(data.shape)
+    clf = svm.SVC(gamma=0.001)
     X_train, X_test, y_train, y_test = train_test_split(
-        data1, digits.target, test_size=split, shuffle=False)
-
-    # Learn the digits on the train subset
-    #print(X_train.shape, y_train.shape)
+        data, Y, test_size=split, shuffle=True)
     y_train = y_train.reshape(-1,1)
     #print(X_train.shape, y_train.shape)
     clf.fit(X_train, y_train)
-
-    # Predict the value of the digit on the test subset
     predicted = clf.predict(X_test)
-
-    ###############################################################################
-    # Below we visualize the first 4 test samples and show their predicted
-    # digit value in the title.
-    '''
-    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-    for ax, image, prediction in zip(axes, X_test, predicted):
-        ax.set_axis_off()
-        image = image.reshape(16, 16)
-        ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-        ax.set_title(f'Prediction: {prediction}')
-    '''
     accuracy = metrics.accuracy_score(y_test, predicted)
-    ###############################################################################
-    # :func:`~sklearn.metrics.classification_report` builds a text report showing
-    # the main classification metrics.
-
-    #print(f"Classification report for classifier {clf}:\n"f"{metrics.classification_report(y_test, predicted)}\n")
-
+    print("{}x{} {}:{} {}".format(int(math.sqrt(data.shape[1])), int(math.sqrt(data.shape[1])), int((1-split)*100),int(split*100), accuracy))
     return accuracy
 
-shape = 256
-split = 0.75
-#print("Accuracy ", fun(digits,shape , split))
-print("16*16   ","75:25   ", fun(digits,shape , split))
+shape = [0.5, 1, 2, 4]
+split = [0.25, 0.5, 0.75]
 
-shape = 256
+for i in shape:
+    for j in split:
+        fun(digits,i , j)
+'''shape = 0.25
+split = 0.25
+#print("Accuracy ", fun(digits,shape , split))
+print("16*16   ","75:25   ", )
+
+shape = 0.5
 split = 0.5
 #print("Accuracy ", fun(digits,shape , split))
 print("16*16   ","50:50   ", fun(digits,shape , split))
 
-shape = 256
+shape = 0.75
 split = 0.25
 #print("Accuracy ", fun(digits,shape , split))
 print("16*16   ","25:75   ", fun(digits,shape , split))
-
-
-shape = 1024    
-split = 0.75
-print("32*32   ","75:25   ", fun(digits,shape , split))
-#print("Accuracy ", fun(digits,shape , split))
-
-shape = 1024    
-split = 0.5
-#print("Accuracy ", fun(digits,shape , split))
-print("32*32    ","50:50   ", fun(digits,shape , split))
-
-shape = 1024
-split = 0.25
-#print("Accuracy ", fun(digits,shape , split))
-print("32*32    ","25:75   ", fun(digits,shape , split))
-
-shape = 4096    
-split = 0.75
-#print("Accuracy ", fun(digits,shape , split))
-print("64*64   ","75:25   ", fun(digits,shape , split))
-
-shape = 4096    
-split = 0.5
-#print("Accuracy ", fun(digits,shape , split))
-print("64*64   ","50:50   ", fun(digits,shape , split))
-
-shape = 4096
-split = 0.25
-#print("Accuracy ", fun(digits,shape , split))
-print("64*64   ","25:75   ", fun(digits,shape , split))
-
-'''
-shape= [256, 1024, 144]
-split = [0.25, 0.50, 0.75]
-
-for i in shape:
-    for j in split:
-        print(shape ,split , fun(digits,shape , split))
 
 '''
