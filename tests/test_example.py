f4b11e88024a37053266de18e43ad8ba1b168cba
JeetRoy97 <royprasenjeet97@gmail.com>
2021-10-07 17:27:58 +0530

    test_overfit and test_model_exists added

--- tests/test_example.py
+++ tests/test_example.py
@@ -10,6 +10,23 @@ def test_equal():
 	assert 1 == 1
 
 def test_model_writing():
+	gamma = 0.001
+	shape = 0.5
+	split = 0.5
+	expected_model_file = "dummy_model"
+	mydir = "/home/jeet/MLOPs/Mnist/mnist/models/"
+	if os.path.exists(mydir):
+		shutil.rmtree(mydir)
+	digits = datasets.load_digits()
+	X,Y = digits.images, digits.target
+	data = utils.preprocess(X, shape)
+	X_train, X_test, y_train, y_test, X_val, y_val = utils.create_splits(data[:100], Y[:100],split)
+	candidate = utils.run_classification_experiment(data, X_train, y_train, X_val, y_val, gamma, split, shape,  mydir, expected_model_file)
+	#print(mydir+expected_model_file)
+	assert os.path.isfile(mydir+expected_model_file+"/model.joblib")
+
+
+def test_small_data_overfit_checking():
 	gamma = 0.01
 	shape = 0.5
 	split = 0.25
