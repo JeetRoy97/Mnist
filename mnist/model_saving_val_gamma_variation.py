ab8e030199ecb3ce149b004d2778a07acf3700bd
JeetRoy97 <royprasenjeet97@gmail.com>
2021-10-07 17:03:21 +0530

    utils file updated in mnist

--- mnist/model_saving_val_gamma_variation.py
+++ mnist/model_saving_val_gamma_variation.py
@@ -9,7 +9,7 @@ import shutil
 from joblib import dump, load
 import numpy as np
 import math
-from utils import create_splits, preprocess, report
+from utils import create_splits, preprocess, report, classification_model
 
 
 digits = datasets.load_digits()
@@ -43,9 +43,12 @@ if os.path.exists(mydir):
     shutil.rmtree(mydir)
 X = digits.images
 Y = digits.target
-split = [0.25, 0.5, 0.75]
-shape = [0.5, 1, 2, 4]
-parames = [1e-7,1e-5,1e-3,0.01,0.1,1]
+#split = [0.25, 0.5, 0.75]
+split = [0.25]
+#shape = [0.5, 1, 2, 4]
+shape = [0.5]
+#parames = [1e-7,1e-5,1e-3,0.01,0.1,1]
+parames = [0.01,0.1]
 acc = []
 checkpoint = []
 model_candidate =[]
@@ -53,15 +56,20 @@ for h in shape:
     for s in split:
         for i in parames:
             data = preprocess(X, h)
-            clf = svm.SVC(gamma=i)
-            X_train, X_test, y_train, y_test, X_val, y_val = create_splits(data, Y,s) # data split function
+            mydir = "/home/jeet/MLOPs/Mnist/mnist/models/" 
+            X_train, X_test, y_train, y_test, X_val, y_val = create_splits(data, Y,s)
+            model_candidate.append(classification_model(data, X_train, y_train, X_val, y_val, i, s, h,  mydir))
+            """clf = svm.SVC(gamma=i)
+             # data split function
             clf.fit(X_train, y_train) 
             model_candidate.append(validate(X_val, y_val,i, s, int(math.sqrt(data.shape[1])))) # validation function
-            mydir = "/home/jeet/MLOPs/Mnist/mnist/models/" 
+            
             output = mydir+"s_{}_tt_{}_val_{}_gamma{}".format(int(math.sqrt(data.shape[1])),s, s, i)
             os.makedirs(output)
             dump(clf, os.path.join(output,"model.joblib"))
-
+        
+"""
+#print(model_candidate)
 
 max_valid = max(model_candidate, key = lambda x: x['acc_valid']) # extract the model based on max accuracy
 
--- mnist/utils.py
+++ mnist/utils.py
@@ -2,6 +2,9 @@ from skimage.transform import rescale, resize, downscale_local_mean
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 import numpy as np
+import math
+import os
+from joblib import dump, load
 
 
 def create_splits(data, Y, split):
@@ -20,3 +23,28 @@ def preprocess(X, shape):
 def report(clf, y_test, predicted):
     print(f"Classification report for classifier {clf}:\n"
       f"{metrics.classification_report(y_test, predicted)}\n")
+
+def validate(data, X_val, y_val, i, s, h,clf):
+    predicted = clf.predict(X_val)
+    accuracy = metrics.accuracy_score(y_val, predicted)
+    if accuracy < 0.11:
+        pass
+        print("Skipping for rescale {}x{} testsize {} gamma {}". format(int(math.sqrt(data.shape[1])), int(math.sqrt(data.shape[1])), s, i))
+    candidate = {
+        'acc_valid' : accuracy,
+        'gamma' : i,
+        'split' : s,
+        'shape' : h
+        }
+    return candidate
+
+def classification_model(data, X_train, y_train, X_val, y_val, gamma, split, shape,  model_path):
+    #model_candidate = []
+    clf = svm.SVC(gamma=gamma)
+    clf.fit(X_train, y_train) 
+    model_candidate = validate(data, X_val, y_val,gamma, split, int(math.sqrt(data.shape[1])),clf) # validation function
+    #print(model_candidate) 
+    output = model_path+"s_{}_tt_{}_val_{}_gamma{}".format(int(math.sqrt(data.shape[1])),split, split, gamma)
+    os.makedirs(output)
+    dump(clf, os.path.join(output,"model.joblib"))
+    return model_candidate
