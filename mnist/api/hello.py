86c4a9e59edae100a756709841c8f0117ec269b8
JeetRoy97 <royprasenjeet97@gmail.com>
2021-11-18 20:52:53 +0530

    hello file updated

--- mnist/api/hello.py
+++ mnist/api/hello.py
@@ -1,7 +1,14 @@
 from flask import Flask
-
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/predict", methods = ['POST'])
+def predict():
+    input_json = request.json
+    image = input_json['image']
+    print(image)
+    return '<p>Image printed</p>'
