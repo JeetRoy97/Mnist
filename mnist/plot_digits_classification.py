cda612bdf364896f328d042c78eb2ce41eab1ca8
JeetRoy97 <royprasenjeet97@gmail.com>
2021-11-18 20:54:33 +0530

    updated

--- mnist/plot_digits_classification.py
+++ mnist/plot_digits_classification.py
@@ -6,6 +6,7 @@ import numpy as np
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 import warnings
+from sklearn import tree
 #from skimage.transform import rescale, resize, downscale_local_mean
 from skimage.transform import rescale
 from sklearn.exceptions import DataConversionWarning
@@ -31,25 +32,29 @@ def rescale_image(X, shape):
     resized_images = resized_images.reshape((len(X), -1))
     return resized_images
 
-def fun(digits, shape, split):
+def fun(digits, shape, split, model):
     X = digits.images
     Y = digits.target
     data = rescale_image(X, shape)
-    #print(data.shape)
-    clf = svm.SVC(gamma=0.001)
     X_train, X_test, y_train, y_test = train_test_split(
         data, Y, test_size=split, shuffle=True)
     y_train = y_train.reshape(-1,1)
-    #print(X_train.shape, y_train.shape)
-    clf.fit(X_train, y_train)
-    predicted = clf.predict(X_test)
+    model.fit(X_train, y_train)
+    predicted = model.predict(X_test)
     accuracy = metrics.accuracy_score(y_test, predicted)
     print("{}x{} {}:{} {}".format(int(math.sqrt(data.shape[1])), int(math.sqrt(data.shape[1])), int((1-split)*100),int(split*100), accuracy))
     return accuracy
 
-shape = [0.5, 1, 2, 4]
-split = [0.25, 0.5, 0.75]
+shape = [1]
+split = [0.25, 0.5, 0.40, 0.60, 0.75, 0.1]
+model1 = svm.SVC(gamma=0.001)
+model2 = tree.DecisionTreeClassifier()
+
 
 for i in shape:
     for j in split:
-        fun(digits,i , j)
+        print("SVM")
+        fun(digits,i , j,model1)
+        print("Decision Tree")
+        fun(digits,i , j,model2)
+        print()
