63f266c245e12aa47e52064a2d6c97f5aa7696d8
JeetRoy97 <royprasenjeet97@gmail.com>
2021-11-27 02:05:55 +0530

    hello.py added

--- mnist/api/hello.py
+++ mnist/api/hello.py
@@ -10,11 +10,22 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-best_model_path = "/home/jeet/MLOPs/Mnist/mnist/models/s_8_tt_0.25_val_0.25_gamma0.01/model.joblib"
-
-@app.route("/predict", methods = ['POST'])
+best_model_path_svm = "/home/jeet/MLOPs/Mnist/mnist/models_train/1.0/split_1.0_gamma0.001/model.joblib"
+best_model_path_decision = "/home/jeet/MLOPs/Mnist/mnist/models_dt/s_8_tt_0.25_val_0.25_depth50/model.joblib"
+@app.route("/svm_predict", methods = ['POST'])
 def predict():
-    clf = load(best_model_path)
+    clf = load(best_model_path_svm)
+    input_json = request.json
+    image = input_json['image']
+    #print(image)
+    #image = np.array(image)
+    image = np.array(image).reshape(1,-1)
+    predicted = clf.predict(image)
+    return str(predicted[0])
+
+@app.route("/decision_predict", methods = ['POST'])
+def decision_predict():
+    clf = load(best_model_path_decision)
     input_json = request.json
     image = input_json['image']
     #print(image)
