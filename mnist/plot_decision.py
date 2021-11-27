b622a514cefc304d77ad820d6968471f429fa98e
JeetRoy97 <royprasenjeet97@gmail.com>
2021-11-27 14:33:45 +0530

    added everything

--- mnist/plot_decision.py
+++ mnist/plot_decision.py
@@ -1,7 +1,7 @@
 from typing import ByteString
 import matplotlib.pyplot as plt
 from skimage.transform import rescale, resize, downscale_local_mean
-from sklearn import datasets, svm, metrics
+from sklearn import datasets, svm, metrics, tree
 from sklearn.model_selection import train_test_split
 import sys
 import os
@@ -14,15 +14,15 @@ from utils import create_splits, preprocess, report, classification_model
 
 digits = datasets.load_digits()
 
-def validate(X_val, y_val, i, s, h):
+def validate(data, X_val, y_val,i, s,h,clf):
     predicted = clf.predict(X_val)
     accuracy = metrics.accuracy_score(y_val, predicted)
     if accuracy < 0.11:
         pass
-        print("Skipping for rescale {}x{} testsize {} gamma {}". format(int(math.sqrt(data.shape[1])), int(math.sqrt(data.shape[1])), s, i))
+        print("Skipping for rescale {}x{} testsize {} depth {}". format(int(math.sqrt(data.shape[1])), int(math.sqrt(data.shape[1])), s, i))
     candidate = {
         'acc_valid' : accuracy,
-        'gamma' : i,
+        'depth' : i,
         'split' : s,
         'shape' : h
         }
@@ -36,9 +36,18 @@ def test(X,best_model_folder, shape,split):
     predicted = clf.predict(X_test)
     report(clf, y_test, predicted) # classification report function
 
+def classification_model_dt(data, X_train, y_train, X_val, y_val, depth, split, shape,  model_path):
+    #model_candidate = []
+    clf = tree.DecisionTreeClassifier(max_depth = depth)
+    clf.fit(X_train, y_train) 
+    model_candidate = validate(data, X_val, y_val,depth, split, int(math.sqrt(data.shape[1])),clf) # validation function
+    #print(model_candidate) 
+    output = model_path+"s_{}_tt_{}_val_{}_depth{}".format(int(math.sqrt(data.shape[1])),split, split, depth)
+    os.makedirs(output)
+    dump(clf, os.path.join(output,"model.joblib"))
+    return model_candidate
 
-
-mydir = "/home/jeet/MLOPs/Mnist/mnist/models/"
+mydir = "/home/jeet/MLOPs/Mnist/mnist/models_dt/"
 if os.path.exists(mydir):
     shutil.rmtree(mydir)
 X = digits.images
@@ -48,17 +57,17 @@ split = [0.25]
 #shape = [0.5, 1, 2, 4]
 shape = [1]
 #parames = [1e-7,1e-5,1e-3,0.01,0.1,1]
-parames = [0.01,0.1]
+depth = [5,10,50,100,500]
 acc = []
 checkpoint = []
 model_candidate =[]
 for h in shape:
     for s in split:
-        for i in parames:
+        for i in depth:
             data = preprocess(X, h)
-            mydir = "/home/jeet/MLOPs/Mnist/mnist/models/" 
+            mydir = "/home/jeet/MLOPs/Mnist/mnist/models_dt/" 
             X_train, X_test, y_train, y_test, X_val, y_val = create_splits(data, Y,s)
-            model_candidate.append(classification_model(data, X_train, y_train, X_val, y_val, i, s, h,  mydir))
+            model_candidate.append(classification_model_dt(data, X_train, y_train, X_val, y_val, i, s, h,  mydir))
             """clf = svm.SVC(gamma=i)
              # data split function
             clf.fit(X_train, y_train) 
@@ -73,6 +82,6 @@ for h in shape:
 
 max_valid = max(model_candidate, key = lambda x: x['acc_valid']) # extract the model based on max accuracy
 
-best_model_folder = "/home/jeet/MLOPs/Mnist/mnist/models/s_{}_tt_{}_val_{}_gamma{}".format(max_valid['shape'],max_valid['split'], max_valid['split'], max_valid['gamma'])
+best_model_folder = "/home/jeet/MLOPs/Mnist/mnist/models_dt/s_{}_tt_{}_val_{}_depth{}".format(max_valid['shape'],max_valid['split'], max_valid['split'], max_valid['depth'])
 
 test(X,best_model_folder, max_valid['shape']/X.shape[1],max_valid['split']) # test function
